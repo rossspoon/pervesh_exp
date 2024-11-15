fad5fe5222e6d784dadf069cce89f4efcd1a4bba
rossspoon <rspoon@vt.edu>
2024-11-15 17:38:03 -0500

    rooms

--- settings.py
+++ settings.py
@@ -46,7 +46,6 @@ ROOMS = [
     dict(
         name='econ101',
         display_name='Econ 101 class',
-        participant_label_file='_rooms/econ101.txt',
     ),
     dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
 ]
